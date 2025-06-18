2910c00ae78102f3b29dbb6d97262ec3234b8dc2
fospacheco <83141359+fospacheco@users.noreply.github.com>
2025-06-18 20:56:52 -0300

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -4,7 +4,7 @@ const firebaseAdmin = initializeApp({
   credential: cert({
     projectId: "cuide-me-f57f8",
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 });
 
