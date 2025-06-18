584994523ea98ea3e68455795ac92e6b6e83f7fe
fospacheco <83141359+fospacheco@users.noreply.github.com>
2025-06-18 19:47:20 -0300

    Delete api/auth directory

--- api/auth/linkedin/login.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const CLIENT_ID = process.env.LINKEDIN_CLIENT_ID;
-  const REDIRECT_URI = process.env.REDIRECT_URI;
-  const SCOPE = "r_liteprofile r_emailaddress";
-
-  const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(SCOPE)}`;
-
-  res.redirect(authUrl);
-}
